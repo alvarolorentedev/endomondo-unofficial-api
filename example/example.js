1f29b98e552d656d3006e310fb22d18c58475753
alvaro <Alvarojosepl@gmail.com>
2017-02-12 09:34:26 -0600

    added posting to endomondo

--- example/example.js
+++ example/example.js
@@ -5,12 +5,14 @@ var params = {
     password: ''
 }
 
-var token = null
+var token = undefined
+var userId = undefined
 
 endomondo.authenticate(params)
     .then((result) => {
         console.log(result)
         token = result.authToken
+        userId = result.userId
         return result
     })
     .then((result) => {return endomondo.workouts({authToken: token})})
@@ -18,7 +20,12 @@ endomondo.authenticate(params)
         console.log(result)
         return result
     })
-    .then((result) => {return endomondo.workout({authToken: token, workoutId: result.data[0].id})})
+    .then((result) => {return endomondo.workout.get({authToken: token, workoutId: result.data[0].id})})
+    .then((result) => {
+        console.log(result)
+        return result
+    })
+    .then((result) => {return endomondo.workout.set({authToken: token, userId: userId, time: Date.now()-70,duration: 60, distance: 0.05 })})
     .then((result) => {
         console.log(result)
         return result
