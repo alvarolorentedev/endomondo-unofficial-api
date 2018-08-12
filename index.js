96028cdccf8911bbf63ad78480855b22af82fe3c
kanekotic <alvarojosepl@gmail.com>
2018-08-12 18:54:46 +0200

    tested index exports

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-'use strict'
+const authenticate = require('./lib/authentication'),
+    workouts = require('./lib/workouts'),
+    workout = require('./lib/workout')
 
-const   authenticate = require('./lib/authentication'),
-        workouts = require('./lib/workouts'),
-        workout = require('./lib/workout')
-
-module.exports= {
-    authenticate: authenticate,
-    workouts: workouts,
-    workout: workout
-}
+module.exports= { authenticate, workouts, workout }
